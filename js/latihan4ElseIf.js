1fe56d870344708bf3f354852755ab400ce5330d
DADev-iD <dilfacakeprock@gmail.com>
2023-10-07 13:20:39 +0700

    menambah materi baru

--- js/latihan4ElseIf.js
+++ js/latihan4ElseIf.js
@@ -5,7 +5,9 @@ let noAngkot = 1;
 for (noAngkot; noAngkot <= jmlAngkot; noAngkot++) {
   if (noAngkot <= angkotBeroperasi) {
     console.log("Angkot No." + noAngkot + " beroperasi dengan baik");
+  } else if (noAngkot === 8) {
+    console.log("Angkot No." + noAngkot + " sedang lembur");
   } else {
-    console.log("Angkot No." + noAngkot + " sedang tidak beroperasi");
+    console.log("Angkot No." + noAngkot + " sedang todak beroperasi");
   }
 }
